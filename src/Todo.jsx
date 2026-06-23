aaa3d660eebebb43b33433468dbf6442cdbed75e
Priyanshi <priyanshjaini1666@gmail.com>
2026-06-23 17:18:09 +0530

    feature

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -2,12 +2,12 @@ import {useState} from 'react';
 import {v4 as uuidv4} from 'uuid';
 
 export default function Todo() {
-    let [todos, setTodos] = useState([{task: "Go to the gym", id: uuidv4()}]);
+    let [todos, setTodos] = useState([{task: "Go to the gym", id: uuidv4(), isDone: false}]);
     let [newTodo, setNewTodo] = useState("");
 
     let addNewTask = () => {
         setTodos((prevTodos)=>{
-            return [...prevTodos,{task: newTodo, id: uuidv4()}]
+            return [...prevTodos,{task: newTodo, id: uuidv4(), isDone: false}]
         });
         setNewTodo("");
     }
@@ -19,6 +19,22 @@ export default function Todo() {
     let deleteTodo = (id) => {
         setTodos((prevTodos) => prevTodos.filter((prevTodos)=> prevTodos.id != id));
     }
+
+    let markAsDone = (id) => {
+        setTodos((prevTodos) => prevTodos.map((todo) =>{
+            if(todo.id == id){
+                return {...todo, isDone: true};
+            }else{
+                return todo;
+            }
+        }))
+    }
+    let markAllAsDone = () =>{
+        setTodos((prevTodos) => prevTodos.map((todo) =>{
+                return {...todo, isDone: true};
+        }))
+
+    }
     
     return (
         <div className="todo-container">
@@ -29,18 +45,23 @@ export default function Todo() {
             </div>
    
             <h2> Todo List </h2>
-            <ul>
+                <ul>
                 {
                     todos.map((todo) => (
                         <li key={todo.id}>
-                        <span>{todo.task}</span>
+                        <span style = {todo.isDone ? {textDecorationLine: "line-through", textDecorationColor: "black"} : {}}>{todo.task}</span>
+                        <button onClick={() => markAsDone(todo.id)}>
+                            Mark as Done
+                        </button>
                         <button onClick={() => deleteTodo(todo.id)}>Delete</button>
                         </li>
-                    )) 
-                }
-            </ul>
+                    ))}
+                </ul>
 
+                <button onClick={() => markAllAsDone()} style ={{ backgroundColor: "#4CAF50", color: "white", border: "none",marginLeft: "130px"}}>
+                    Mark  all as Done
+                </button>
         </div>
-    )
+    );
 
 }
